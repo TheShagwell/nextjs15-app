fd38a50d86b1ae214dcab4c5f88eaa7279bf400e
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2025-10-28 18:16:39 +0100

    updated...

--- app/users/[userId]/not-found.tsx
+++ app/users/[userId]/not-found.tsx
@@ -1,12 +1,12 @@
 export default function LoadingSpinner() {
   return (
-    <div>
+    <>
       <div className="flex items-center justify-center min-h-screen">
         <div className="className flex flex-col gap-2">
-          <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-blue-500"></div>
+          <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-blue-800"></div>
           <p className="text-center text-gray-200">Not Found...</p>
         </div>
       </div>
-    </div>
+    </>
   );
 }
